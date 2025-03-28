bb4749fc138cfb875d19ed08b32571c23959bbb5
tiago-sb <belatiago72@gmail.com>
2025-03-28 12:57:35 -0300

    atualização

--- src/components/about/teaches/Instrutores.jsx
+++ src/components/about/teaches/Instrutores.jsx
@@ -34,7 +34,7 @@ const Instrutores = () => {
           </Col>
           <Col>
             <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="./assets/images/teaches_images/t4.jpg" />
+              <Card.Img variant="top" src="/assets/images/teaches_images/t4.jpg" />
               <Card.Body>
                 <Card.Title style={{ fontFamily: 'Bebas Neue' }}>Carla - Salvador</Card.Title>
                 <Card.Text style={{ fontFamily: 'Poppins' }}>
@@ -48,7 +48,7 @@ const Instrutores = () => {
           <Col>
             <Card style={{ width: '18rem' }}>
               <Card.Img variant="top" src="../../../../public/assets/images/teaches_images/t5.jpg" />
-              <Card.Body>
+              <Card.Body> 
                 <Card.Title style={{ fontFamily: 'Bebas Neue' }}>Romário - São Paulo</Card.Title>
                 <Card.Text style={{ fontFamily: 'Poppins' }}>
                   Boxe, Cardio
